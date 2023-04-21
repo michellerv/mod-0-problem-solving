bd8b1a7534e989048334243edb8b471ec901a168
Michelle Villagran <mrvillagran16@gmail.com>
2023-04-21 11:32:40 -0600

    Add problem solving process

--- challenge1.js
+++ challenge1.js
@@ -1,4 +1,14 @@
 //1. Given an array of strings, 
 //print only the strings that have exactly 4 characters.
 
-//create array with characters that have 
+
+//create array with strings that have at least a few with exactly four letters
+//data is an array of strings
+//nothing unclear
+
+// create a for statment that goes through the array 
+// the strings that have exactly 4 lettersuld be printed.
+// going to research for statement syntax again
+
+
+var words = ["like", "happy", "cats", "loopy"]
