0bd3d93f26b71da60efc59d1fefaa859ae4f254e
Michelle Villagran <mrvillagran16@gmail.com>
2023-04-21 12:56:20 -0600

    Write code for challenge 4, driving

---
+++ challenge4.js
@@ -0,0 +1,9 @@
+var destinations = ["New York", "Miami", "Austin", "Denver"] 
+
+for ( var i = 0; i < destinations.length; i++ ) {
+    
+    
+console.log(`The next place I want to visit is ${destinations.sort()[i]}`)
+}
+
+
