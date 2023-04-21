df462d149d615441f9980ce28d1a72ff30b5f1f8
Michelle Villagran <mrvillagran16@gmail.com>
2023-04-21 13:08:45 -0600

    add pseudo code throughout code

--- challenge3.js
+++ challenge3.js
@@ -4,12 +4,21 @@
 //string as an argument and return the same string with every 
 //instance of the letter <strong>s</strong> removed.
 
+//Create a string that has an s in it.
+// Create a method or fuction that takes in the string, and removes s.
+// The argument is the s, and the paramater is the string.
 
+// research functions to replace s with nothing.
 
 var comment = ("hey sweetie, you got this")
+// creating the string variable
 function removeS(comment) {
+    //this function we created is named and will act on the string.
+
     return comment.replaceAll('s', '')
+    //the return of the function should relace s with nothing.
 
 } 
 
 console.log(removeS(comment))
+// print the string with the function in place. 
