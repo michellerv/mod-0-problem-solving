1845caae086a42745d8f5a216ec8cfa7105b2b57
Michelle Villagran <mrvillagran16@gmail.com>
2023-04-21 11:41:30 -0600

    Complete challenge 1

--- challenge1.js
+++ challenge1.js
@@ -1,14 +1,24 @@
 //1. Given an array of strings, 
 //print only the strings that have exactly 4 characters.
 
-
+//Overall goal
 //create array with strings that have at least a few with exactly four letters
 //data is an array of strings
 //nothing unclear
 
+//Pseudocode
 // create a for statment that goes through the array 
-// the strings that have exactly 4 lettersuld be printed.
+// the strings that have exactly 4 letters should be printed.
 // going to research for statement syntax again
+// create an if statement to  only print strings with 4 characters
 
+// final solution
 
 var words = ["like", "happy", "cats", "loopy"]
+
+for (var i = 0; i < words.length; i++) {
+    if (words[i].length === 4) {
+
+    console.log((words[i]))
+    }
+}
